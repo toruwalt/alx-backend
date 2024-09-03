27c7c6ec828bfcbf8f9ef8dca081ec0ac750365a
Torubiri Walter <toruwalt1997@gmail.com>
2024-09-03 14:34:01 +0100

    Caching - Task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Basic caching module.
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Inherited Cache Class"""
+    def init(self):
+        """Initialization method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Method to put item into cache at index key"""
+        if type(key) is None or type(item) is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Method to get item from cache at index key"""
+        if key in self.cache_data:
+            return self.cache_data[key]
