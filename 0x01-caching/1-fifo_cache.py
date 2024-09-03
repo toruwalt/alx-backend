4d1651b38f7e91371407199d94e23083f4a2a340
Torubiri Walter <toruwalt1997@gmail.com>
2024-09-03 16:09:37 +0100

    Caching - Task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Basic FIFO Caching System.
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Inherited Cache Class"""
+    def init(self):
+        """Initialization method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Method to put item into cache at index key"""
+        if type(key) is None or type(item) is None:
+            pass
+        else:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    item_del = next(iter(self.cache_data.keys()))
+                    del self.cache_data[item_del]
+                    self.cache_data[key] = item
+                    print("DISCARD {}".format(item_del))
+                else:
+                    self.cache_data[key] = item
+
+    def get(self, key):
+        """Method to get item from cache at index key"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+
+
