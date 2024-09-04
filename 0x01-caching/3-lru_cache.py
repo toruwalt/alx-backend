0434145014543c5b77821a4127fe5297339cf0e0
Torubiri Walter <toruwalt1997@gmail.com>
2024-09-04 21:56:43 +0100

    Caching - Task 3:Attempt 4

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Basic LRU Caching System.
+"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Inherited Cache Class"""
+    def __init__(self):
+        """Initialization method"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+        self.least_used = {}
+
+    def put(self, key, item):
+        """Method to put item into cache at index key"""
+        if key is None or item is None:
+            return
+        else:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.cache_data.move_to_end(key, True)
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    del_item_key, val = self.cache_data.popitem(last=False)
+                    self.cache_data[key] = item
+                    print("DISCARD {}".format(del_item_key))
+                else:
+                    self.cache_data[key] = item
+
+    def get(self, key):
+        """Method to get item from cache at index key"""
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, True)
+            return self.cache_data.get(key, None)
