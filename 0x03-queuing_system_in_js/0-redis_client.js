5d4e868d7f45d4a3f79800cc8147a4585e38d936
Torubiri Walter <toruwalt1997@gmail.com>
2024-10-07 16:01:51 +0100

    Queuing System in JS - Task 0:Attempt 2

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,10 @@
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server: ' + err);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
